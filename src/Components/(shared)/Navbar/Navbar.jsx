ab46eb3644a953baa9c0c884d08e0ec49a31d829
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-14 15:18:18 +0600

    navbar for next js or react js

--- src/Components/(shared)/Navbar/Navbar.jsx
+++ src/Components/(shared)/Navbar/Navbar.jsx
@@ -1,29 +1,81 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState(false);
     return (
-        <div className="navbar bg-base-100">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] px-4 py-2 shadow bg-base-100 rounded-box w-52 flex flex-col gap-3 ">
-                        {navOptions}
-                    </ul>
+        <nav className="w-full  ">
+            <div className="justify-between mx-auto  md:items-center md:flex px-4">
+                <div>
+                    <div className="flex items-center justify-between py-3  md:block">
+                        <div href="javascript:void(0)">
+                            <Link href="/" className="text-3xl logo font-bold">Insight</Link>
+                        </div>
+                        <div className="md:hidden">
+                            <button
+                                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                                onClick={() => setNavbar(!navbar)}  >
+                                {navbar ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        stroke="currentColor"
+                                        strokeWidth={2}
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <div>
+                    <div
+                        className={`flex-1 justify-self-center pb-3  md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                            }`}  >
+                        <ul className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0 md:flex-row ">
+                            <li><Link href="/" className="text-gray-600 hover:text-[#35bdcd]">
+                                Home
+                            </Link></li>
+                            <li><Link href="/blog" className="text-gray-600 hover:text-[#35bdcd]">
+                                Blog
+                            </Link></li>
+                            <li><Link href="/" className="text-gray-600 hover:text-[#35bdcd]">
+                                About
+                            </Link></li>
+                            <li><Link href="/" className="text-gray-600 hover:text-[#35bdcd]">
+                                Contact
+                            </Link></li>
+                            <li><Link href="/" className="text-gray-600 hover:text-[#35bdcd]">
+                                Dashboard
+                            </Link></li>
+
+                        </ul>
+                    </div>
+                </div>
             </div>
-            {/* desktop */}
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1 flex gap-10">
-                    {navOptions}
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
-        </div>
+        </nav>
+
     );
 };
 
@@ -31,10 +83,4 @@ export default Navbar;
 
 
 
-const navOptions = <>
 
-    <Link href='/'>Home</Link>
-    <Link href='/blog'>Blog</Link>
-    <Link href='/contact'>Contact</Link>
-
-</>
