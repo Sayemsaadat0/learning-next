6cd5c9ba3da8d1255948f2b2603452fb1d98b891
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-14 14:49:49 +0600

    navbar footar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,11 @@
 
 export default function Home() {
   return <div>
+
+
+    <h1 >This is h1</h1>
+    <h2>this is h2</h2>
+    <h3>this is h3</h3>
     <p> hello world this might be my last video</p>
   </div>;
 }
