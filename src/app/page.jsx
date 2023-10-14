107728570a338167d784f0ab9d8823f2542e6d8d
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-10-14 13:33:43 +0600

    pages creating

---
+++ src/app/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return <div>
+    <p> hello world this might be my last video</p>
+  </div>;
+}
---
+++ src/app/portfulio/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Portfulio = () => {
+    return (
+        <div>
+            Portfulio
+        </div>
+    );
+};
+
+export default Portfulio;
